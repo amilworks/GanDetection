4bb6e1eace3f1f728cee92676b0cbfe7b7215444
Amil Khan <amil@ucsb.edu>
2021-02-11 18:40:23 -0800

    Update make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -11,6 +11,7 @@ from dotenv import find_dotenv, load_dotenv
 def main(input_filepath, output_filepath):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
+        NOTE: Still a work in progress! HMU if you need this piece done!
     """
     logger = logging.getLogger(__name__)
     logger.info('making final data set from raw data')
